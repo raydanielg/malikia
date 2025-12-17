bf95447af140e0aa84b932e28aa385ee912e0381
raydanielg <airezra2+1@gmail.com>
2025-12-17 21:43:46 +0300

    commits

--- resources/views/panel/layouts/app.blade.php
+++ resources/views/panel/layouts/app.blade.php
@@ -14,16 +14,16 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-gray-50 dark:bg-gray-800 min-h-screen">
+<body class="bg-gray-100 min-h-screen">
     @include('panel.layouts.sidebar')
 
-    <div class="p-4 sm:ml-80 flex flex-col min-h-screen">
+    <div class="sm:ml-80 flex flex-col min-h-screen">
         {{-- Top Header / Navbar --}}
         @include('panel.layouts.header')
 
         {{-- Main Content Wrapper --}}
-        <main class="mt-4 flex-1">
-            <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 bg-white/60 backdrop-blur-sm">
+        <main class="mt-4 flex-1 px-4 pb-6">
+            <div class="bg-white rounded-xl shadow-sm border border-gray-200 p-4 sm:p-6">
                 @yield('content')
             </div>
         </main>
