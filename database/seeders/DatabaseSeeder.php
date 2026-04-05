e4c62fb84b81ec9075e196d2255cc63bc4457ee1
raydanielg <airezra2+1@gmail.com>
2026-04-05 23:31:30 +0300

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         // );
 
         $this->call([
+            RegionDistrictSeeder::class,
             TestimonialSeeder::class,
             UserDemoSeeder::class,
         ]);
